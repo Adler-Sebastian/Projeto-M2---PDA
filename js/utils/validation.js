fa646451426f3bf417ecf3645a154acc39308c9f
brenddamachado <brenda16machado.bm@gmail.com>
2024-04-02 14:52:24 -0300

    validação do cep e mascara do numero de celular

--- js/utils/validation.js
+++ js/utils/validation.js
@@ -1,8 +1,8 @@
 const cpf = document.getElementById("cpf");
 const cpfMessage = document.getElementById("cpfMessage");
+const number = document.getElementById("number");
 
 cpf.addEventListener("input", cpfValidation);
-
 function cpfValidation() {
   const cpfInput = cpf.value;
   const cpfDigits = cpfInput.replace(/\D/g, "");
@@ -23,33 +23,103 @@ function cpfValidation() {
     cpfInput === "88888888888" ||
     cpfInput === "99999999999"
   ) {
-    cpfMessage.innerHTML = "Não repita os números.";  
+    cpfMessage.innerHTML = "Não repita os números.";
 
-  return false;
-}
-  let soma = 0;
-  let resto;
+    return false;
+  }
+  let sum = 0;
+  let rest;
 
   for (let i = 0; i < 9; i++) {
-    soma = soma + parseInt(cpfInput.charAt(i)) * (10 - i);
+    sum = sum + parseInt(cpfInput.charAt(i)) * (10 - i);
   }
-  resto = (soma * 10) % 11;
-  if (resto == 10 || resto == 11) resto = 0;
-  if (resto != parseInt(cpfInput.charAt(9))) {
+  rest = (sum * 10) % 11;
+  if (rest == 10 || rest == 11) rest = 0;
+  if (rest != parseInt(cpfInput.charAt(9))) {
     cpfMessage.innerHTML = "inválido.";
     return;
   }
 
-  soma = 0;
+  sum = 0;
   for (let i = 0; i < 10; i++) {
-    soma = soma + parseInt(cpfInput.charAt(i)) * (11 - i);
+    sum = sum + parseInt(cpfInput.charAt(i)) * (11 - i);
   }
-  resto = (soma * 10) % 11;
-  if (resto == 10 || resto == 11) resto = 0;
-  if (resto != parseInt(cpfInput.charAt(10))) {
+  rest = (sum * 10) % 11;
+  if (rest == 10 || rest == 11) rest = 0;
+  if (rest != parseInt(cpfInput.charAt(10))) {
     cpfMessage.innerHTML = " inválido.";
     return;
   }
 
   cpfMessage.innerHTML = "CPF válido.";
 }
+
+$(document).ready(function () {
+  //celular
+  $("#number").mask("(99) 99999-9999");
+});
+
+zipCode.addEventListener("input", endereco);
+
+function endereco() {
+  const zipCode = document.getElementById("zipCode").value;
+  const city = document.getElementById("city");
+  const neighborhood = document.getElementById("neighborhood");
+  const road = document.getElementById("road");
+
+  if (zipCode.length >8) {
+    alert("CEP inválido!");
+    return;
+  }
+
+  const url = `https://viacep.com.br/ws/${zipCode}/json/`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      city.value = data.localidade;
+      road.value = data.logradouro;
+      neighborhood.value = data.bairro;
+    })
+    .catch(error => {
+      console.error("Erro ao buscar endereço:", error);
+      // Exibir mensagem de erro para o usuário
+    });
+}
+
+// number.addEventListener("input", celular);
+// function celular(number) {
+//   if (number.length !== 15) return false;
+//   if (
+//     number[0] !== "(" ||
+//     number[3] !== ")" ||
+//     number[4] !== " " ||
+//     number[10] !== "-"
+//   )
+//     return false;
+//   // Verificar se os caracteres restantes são dígitos
+//   for (let i = 1; i < number.length; i++) {
+//     if (i === 3 || i === 4 || i === 10) continue; // Pula os caracteres que já foram verificados
+//     if (number[i] < "0" || number[i] > "9") return false;
+//   }
+
+//   // Se passou por todas as verificações, o formato é válido
+//   return true;
+// }
+// var linkSession = document.getElementById('session');
+// var formCadastro = document.querySelector('main'); // Supondo que você quer esconder todo o <main>
+// var formLogin = document.getElementById('login4');
+
+// linkSession.addEventListener('click', function(e) {
+//   e.preventDefault(); // Impede o comportamento padrão do link
+//   formCadastro.style.display = 'none'; // Esconde o form de cadastro
+//   formLogin.style.display = 'block'; // Mostra o form de login
+// });
+
+// // Opcional: Adicione um botão ou link no seu form de login para voltar ao cadastro
+// // Aqui estou assumindo que você adicionou um botão com id="voltarCadastro"
+// document.getElementById('voltarCadastro').addEventListener('click', function(e) {
+//   e.preventDefault();
+//   formLogin.style.display = 'none'; // Esconde o form de login
+//   formCadastro.style.display = 'block'; // Mostra o form de cadastro novamente
+// });
