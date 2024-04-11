dbe719fddd83a668f59ca4a3a33e78690bf9c023
Adler Sebastian <adlersebas91@gmail.com>
2024-04-11 11:11:18 -0300

    Adição de comentário

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ menuIcon.addEventListener("click", () => {
     header.classList.toggle("show-mobile-menu");
 });
 
+// Script de redirecionamento ao login
+
 let register=document.getElementById("register"
 )
 register.addEventListener("click",function(){
